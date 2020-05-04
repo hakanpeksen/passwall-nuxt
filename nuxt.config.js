ab587797728331df08554724f76d478b43d21293
hakanpeksen <hknpksn@gmail.com>
2020-05-04 22:39:20 +0300

    fontawesome,bulma,buefy eklendi.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,71 @@
-
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma',
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false
-        }
-      }
+    mode: 'universal',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [
+        { src: '~/assets/css/main.scss', lang: 'scss' }
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        'nuxt-buefy',
+        'nuxt-fontawesome'
+        // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
+
+    ],
+    buefy: {
+        materialDesignIcons: false,
+        defaultIconPack: 'fas',
+        defaultIconComponent: 'font-awesome-icon'
+    },
+    fontawesome: {
+        imports: [{
+            set: '@fortawesome/free-solid-svg-icons',
+            icons: ['fas']
+        }]
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        // postcss: {
+        //     preset: {
+        //         features: {
+        //             customProperties: false
+        //         }
+        //     }
+        // },
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="container">
-      <p>Login</p>
+ <div class="container">
+          <section>
+        <button class="button block" @click="isActive = !isActive">Toggle</button>
+        <b-message title="Default" :active.sync="isActive" aria-close-label="Close message">
+            Lorem ipsum dolor 
+        </b-message>
+    </section>
+
+     <b-field label="Password">
+            <b-input type="password"
+                value="iwantmytreasure"
+                password-reveal>
+            </b-input>
+        </b-field>
+
+
+          <button class="button is-medium is-success" @click="success">
+                Launch toast (custom)
+            </button>
+<b-icon pack="fas" icon="home" size="is-large" />
+<b-icon pack="fas" icon="user" size="is-large" />
+<b-icon pack="fas" icon="lock" size="is-medium" />
   </div>
-</template>
+</template> 
  
 <script>
 import Logo from '~/components/Logo.vue'
 
 export default {
+    data() {
+            return {
+                isActive: true
+            }
+        },
+          methods: {         
+            success() {
+                this.$buefy.toast.open({
+                    message: 'Something happened correctly!',
+                    type: 'is-success'
+                })
+            }
+          },
   components: {
     Logo
   }
@@ -15,12 +48,5 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+
 </style>
