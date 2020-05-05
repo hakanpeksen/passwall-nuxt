63e397a10f161fcc0303361fa9427169cc070eb5
hakanpeksen <hknpksn@gmail.com>
2020-05-05 10:42:47 +0300

    npm run lint  ve  npm run lintfix  çalışır

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,71 +1,83 @@
 export default {
-    mode: 'universal',
-    /*
-     ** Headers of the page
-     */
-    head: {
-        title: process.env.npm_package_name || '',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-    /*
-     ** Customize the progress-bar color
-     */
-    loading: { color: '#fff' },
-    /*
-     ** Global CSS
-     */
-    css: [
-        { src: '~/assets/css/main.scss', lang: 'scss' }
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [{ src: '~/assets/css/main.scss', lang: 'scss' }],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/axios',
+    'nuxt-buefy',
+    'nuxt-fontawesome'
+    // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
+  ],
+  buefy: {
+    materialDesignIcons: false,
+    defaultIconPack: 'fas',
+    defaultIconComponent: 'font-awesome-icon'
+  },
+  fontawesome: {
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      }
+    ]
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    // postcss: {
+    //     preset: {
+    //         features: {
+    //             customProperties: false
+    //         }
+    //     }
+    // },
     /*
-     ** Plugins to load before mounting the App
+     ** You can extend webpack config here
      */
-    plugins: [],
-    /*
-     ** Nuxt.js dev-modules
-     */
-    buildModules: [],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-        'nuxt-buefy',
-        'nuxt-fontawesome'
-        // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
-
-    ],
-    buefy: {
-        materialDesignIcons: false,
-        defaultIconPack: 'fas',
-        defaultIconComponent: 'font-awesome-icon'
-    },
-    fontawesome: {
-        imports: [{
-            set: '@fortawesome/free-solid-svg-icons',
-            icons: ['fas']
-        }]
-    },
-    /*
-     ** Build configuration
-     */
-    build: {
-        // postcss: {
-        //     preset: {
-        //         features: {
-        //             customProperties: false
-        //         }
-        //     }
-        // },
-        /*
-         ** You can extend webpack config here
-         */
-        extend(config, ctx) {}
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
     }
+  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,105 @@
 <template>
- <div class="container">
-          <section>
-        <button class="button block" @click="isActive = !isActive">Toggle</button>
-        <b-message title="Default" :active.sync="isActive" aria-close-label="Close message">
-            Lorem ipsum dolor 
-        </b-message>
+  <div class="container">
+    <section>
+      <button
+        @click="isActive = !isActive"
+        class="button block"
+      >
+        Toggle
+      </button>
+      <b-message
+        :active.sync="isActive"
+        title="Default"
+        aria-close-label="Close message"
+      >
+        Lorem ipsum dolor
+      </b-message>
     </section>
+    <b-field label="Password">
+      <b-input
 
-     <b-field label="Password">
-            <b-input type="password"
-                value="iwantmytreasure"
-                password-reveal>
-            </b-input>
-        </b-field>
+      
+        type="password"
+        value="iwantmytreasure"
+        password-reveal
+      />
+    </b-field>
 
-
-          <button class="button is-medium is-success" @click="success">
-                Launch toast (custom)
-            </button>
-<b-icon pack="fas" icon="home" size="is-large" />
-<b-icon pack="fas" icon="user" size="is-large" />
-<b-icon pack="fas" icon="lock" size="is-medium" />
+    <button
+      @click="success"
+      class="button is-medium is-success"
+    >
+      Launch toast (custom)
+    </button>
+    <b-icon
+      pack="fas"
+      icon="home"
+      size="is-large"
+    />
+    <b-icon
+      pack="fas"
+      icon="user"
+      size="is-large"
+    />
+    <b-icon
+      pack="fas"
+      icon="lock"
+      size="is-medium"
+    />
+    <b-icon
+      pack="fas"
+      icon="tachometer-alt"
+      size="is-medium"
+    />
+    <b-icon
+      pack="fas"
+      icon="check"
+    />
   </div>
-</template> 
- 
-<script>
-import Logo from '~/components/Logo.vue'
+</template>
 
+<script>
 export default {
-    data() {
-            return {
-                isActive: true
-            }
-        },
-          methods: {         
-            success() {
-                this.$buefy.toast.open({
-                    message: 'Something happened correctly!',
-                    type: 'is-success'
-                })
-            }
-          },
-  components: {
-    Logo
+  components: {},
+  data() {
+    return {
+      isActive: true
+    }
+  },
+  created() {
+    this.signinUser()
+  },
+  methods: {
+    success() {
+      this.$buefy.toast.open({
+        message: 'Something happened correctly!',
+        type: 'is-success'
+      })
+    },
+
+    async signinUser() {
+      try {
+        //  await this.$axios.get({ data: this.userForm })
+        await this.$axios
+          .$get('https://jsonplaceholder.typicode.com/posts')
+          .then((res
+
+          ) => {
+            console.log(res)
+          })
+        //   this.$toast.open({
+        //   message: 'You have successfully logged in!',
+        //   type: 'is-success',
+        //   position: 'is-bottom',
+        //   duration: 7000
+        // })
+      } catch (e) {
+        //   this.errorMessage = `Your email address or password don't match!`
+        //  this.isError = true
+      }
+    }
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
