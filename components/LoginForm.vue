38522bc7c0424b5f837c6c747e26ba6742960386
hakanpeksen <hknpksn@gmail.com>
2020-05-07 11:40:27 +0300

    http://d  yazdığın anda  baseurl  valid yapıyor

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -49,8 +49,8 @@
               password-reveal
             />
           </b-field>
-          <div class="field">
-            <button class="button is-info">
+          <div class="control">
+            <button type="submit" name="button" class="button is-medium is-info">
               Login
             </button>
           </div>
@@ -82,8 +82,14 @@ export default {
   //   },
   methods: {
     async signinUser() {
+      const url = this.userForm.baseurl
+      const urlRegExp = /^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/ // eslint-disable-line max-len
+      if (!urlRegExp.test(url)) {
+        alert('BaseURL must be a valid URL')
+      }
       try {
         await this.$axios.post('https://passwall-server.hakanpeksen.com/auth/signin', this.userForm)
+        this.isError = false
         this.$buefy.toast.open({
           message: 'You have successfully logged in!',
           type: 'is-success',
